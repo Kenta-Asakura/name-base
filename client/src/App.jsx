da91d0259568fb367a3c9c4c0754d6b005ad6466
kenta asakura <asakura.kenta963420@gmail.com>
2025-04-17 15:08:33 +0900

    Deleted default react files (styling, assets)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
      <div className="min-h-screen bg-base-200 flex items-center justify-center">
@@ -30,4 +25,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
