ba808c78d1674fcb4c412bca5c6a793102bfd933
kenta asakura <asakura.kenta963420@gmail.com>
2025-04-20 16:21:55 +0900

    setup and test backend server with Hono, connected the Backend to the DB, and created a simple route for a test SQL query

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,36 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
-import cors from 'cors';
+import postgres from "postgres";
+import dotenv from 'dotenv';
+// import { cors } from 'hono/cors';
+
+// Load environment variables
+dotenv.config();
+
+// * TEST ENV
+// const result = dotenv.config();
+// console.log(
+//   "ENV loaded:", result,
+//   "DB_USER:", process.env.DB_USER
+// );
+
+// * TEST connection
+const sql = postgres({
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || "5432"),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 const app = new Hono();
 
-app.use('*', cors()); // enable CORS for all routes (optional)
-
-app.get('/', (c) => {
-  return c.text('Backend is running!')
+app.get("/", async (c) => {
+  const result = await sql`SELECT 1 + 1`;
+  console.log(result);
+  return c.text("Hello!");
 });
 
-serve(app);
+serve(app, (info) => {
+  console.log(`Listening on http://localhost:${info.port}`);
+});
