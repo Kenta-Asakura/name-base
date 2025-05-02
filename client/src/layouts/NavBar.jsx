58062424d6b80ef6c8c23946a15497875728ac6a
kenta asakura <asakura.kenta963420@gmail.com>
2025-05-02 11:54:18 +0900

    Auth0 Navbar and dropdown UI improved

--- client/src/layouts/NavBar.jsx
+++ client/src/layouts/NavBar.jsx
@@ -20,7 +20,7 @@ function Navbar({ toggleForm }) {
         <a className="btn btn-ghost text-xl">NameBase</a>
       </div>
 
-      <div className="flex-none gap-2">
+      <div className="flex gap-2">
         {isAuthenticated && (
           <button
             onClick={toggleForm}
@@ -32,11 +32,25 @@ function Navbar({ toggleForm }) {
 
         {!isLoading && (
           <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
-              </svg>
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+              {isAuthenticated ? (
+                <div className="w-10 rounded-full">
+                  <img
+                    src={user?.picture}
+                    alt={`${user?.name}'s profile`}
+                    onError={(e) => {
+                      e.target.onerror = null;
+                      e.target.src = "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg";
+                    }}
+                  />
+                </div>
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
+                </svg>
+              )}
             </div>
+
             <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
               {isAuthenticated ? (
                 <>
@@ -46,13 +60,12 @@ function Navbar({ toggleForm }) {
                       <span className="badge">User</span>
                     </a>
                   </li>
-                  <li><a>{user?.email}</a></li>
                   <li><LogoutButton /></li>
                 </>
               ) : (
                 <>
                   <li><LoginPopupButton /></li>
-                  <li><LoginRedirectButton></LoginRedirectButton></li>
+                  <li><LoginRedirectButton /></li>
                 </>
               )}
             </ul>
