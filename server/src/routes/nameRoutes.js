7bc595c2d081514f69d921455e75b22eb34bdeb3
Kenta Asakura <asakura.kenta963420@gmail.com>
2025-09-02 22:53:24 +0900

    nameRoutes code cleanup (code readability improvement and test lines deleted)

--- server/src/routes/nameRoutes.js
+++ server/src/routes/nameRoutes.js
@@ -1,11 +1,8 @@
 import { Hono } from "hono";
 import { nameController } from "../controllers/nameController.js";
 import { jwtMiddleware } from "../middleware/auth.js";
-
-// ! TEST
 import { requirePermission } from "../middleware/permissions.js";
 
-// ! TEST
 const nameRouter = new Hono();
 
 nameRouter.get('/', nameController.getAllNames);
@@ -16,15 +13,36 @@ nameRouter.get('/', nameController.getAllNames);
 // nameRouter.put('/:id', jwtMiddleware, nameController.updateName);
 // nameRouter.delete('/:id', jwtMiddleware, nameController.deleteName);
 
-
 // - Protected routes with specific permissions (Auth0)
-// ! TEST
-// nameRouter.get('/', requirePermission('read:names'), nameController.getAllNames);
-nameRouter.get('/', jwtMiddleware, requirePermission('read:names'), nameController.getAllNames);
+nameRouter.get(
+    '/', 
+    jwtMiddleware, 
+    requirePermission('read:names'), 
+    nameController.getAllNames
+);
+
 // * To be implemented
 // nameRouter.get('/:id', jwtMiddleware, requirePermission(), nameController.getNameById);
-nameRouter.post('/', jwtMiddleware, requirePermission('create:names'), nameController.createName);
-nameRouter.put('/:id', jwtMiddleware, requirePermission('update:names'), nameController.updateName);
-nameRouter.delete('/:id', jwtMiddleware, requirePermission('delete:names'), nameController.deleteName);
+
+nameRouter.post(
+    '/', 
+    jwtMiddleware, 
+    requirePermission('create:names'), 
+    nameController.createName
+);
+
+nameRouter.put(
+    '/:id',
+    jwtMiddleware, 
+    requirePermission('update:names'), 
+    nameController.updateName
+);
+
+nameRouter.delete(
+    '/:id', 
+    jwtMiddleware, 
+    requirePermission('delete:names'), 
+    nameController.deleteName
+);
 
 export default nameRouter;
